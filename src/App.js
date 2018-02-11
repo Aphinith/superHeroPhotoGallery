6aa52916b6a25d7afe13ba48c4815448a7153b14
Aralya Phinith <aphinith84@gmail.com>
2018-02-11 14:13:23 -0800

    initial commit for intial setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import SuperHeroesList from './SuperHeroesList';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Super Hero Photo Gallery</h1>
+        </header>
+        <p className="App-intro">
+          Below are all of the super heroes you have requested.
+        </p>
+        <SuperHeroesList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/SuperHeroesList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SuperHeroesList.css';
+
+class SuperHeroesList extends Component {
+
+
+  render() {
+    return (
+      <div>
+        This is the component that will render all the super heroes!
+      </div>
+    );
+  }
+}
+
+export default SuperHeroesList;
