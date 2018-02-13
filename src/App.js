850df8d4e8084d129f1acf610bbbad9f680977a6
Aralya Phinith <aphinith84@gmail.com>
2018-02-12 20:21:32 -0800

    clean up files, remove console log statements, add alt attribute to img tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SuperHeroesList from './SuperHeroesList';
-import logo from './logo.svg';
 import marvelLogo from './marvel-logo.png';
 import './App.css';
 
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Super Hero Photo Gallery</h1>
           <img src={marvelLogo} className="marvel-logo" alt="logo" />
         </header>
--- src/SuperHeroesList.js
+++ src/SuperHeroesList.js
@@ -66,7 +66,7 @@ class SuperHeroesList extends Component {
               {hero.name}
             </div>
             <div className="hero-image-container" data-index={index}>
-              <img src={hero.imageUrl} className="hero-imageurl" data-index={index}/>
+              <img src={hero.imageUrl} className="hero-imageurl" data-index={index} alt="pic"/>
             </div>
           </div>
         );
@@ -80,7 +80,6 @@ class SuperHeroesList extends Component {
 
   render() {
     if (this.state.allSuperHeroes.length > 0) {
-      console.log('this.slide.index: ', this.state.slideIndex);
       const allSuperHeroes = this.state.allSuperHeroes;
       return (
         <div>
@@ -99,102 +98,102 @@ class SuperHeroesList extends Component {
                 <Slider>
                   <Slide index={0}>
                     <div className="carousel-hero-name">{allSuperHeroes[0]['name']}</div>
-                    <img src={allSuperHeroes[0]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[0]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[0]['description']}</div>
                   </Slide>
                   <Slide index={1}>
                     <div className="carousel-hero-name">{allSuperHeroes[1]['name']}</div>
-                    <img src={allSuperHeroes[1]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[1]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[1]['description']}</div>
                   </Slide>
                   <Slide index={2}>
                     <div className="carousel-hero-name">{allSuperHeroes[2]['name']}</div>
-                    <img src={allSuperHeroes[2]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[2]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[2]['description']}</div>
                   </Slide>
                   <Slide index={3}>
                     <div className="carousel-hero-name">{allSuperHeroes[3]['name']}</div>
-                    <img src={allSuperHeroes[3]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[3]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[3]['description']}</div>
                   </Slide>
                   <Slide index={4}>
                     <div className="carousel-hero-name">{allSuperHeroes[4]['name']}</div>
-                    <img src={allSuperHeroes[4]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[4]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[4]['description']}</div>
                   </Slide>
                   <Slide index={5}>
                     <div className="carousel-hero-name">{allSuperHeroes[5]['name']}</div>
-                    <img src={allSuperHeroes[5]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[5]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[5]['description']}</div>
                   </Slide>
                   <Slide index={6}>
                     <div className="carousel-hero-name">{allSuperHeroes[6]['name']}</div>
-                    <img src={allSuperHeroes[6]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[6]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[6]['description']}</div>
                   </Slide>
                   <Slide index={7}>
                     <div className="carousel-hero-name">{allSuperHeroes[7]['name']}</div>
-                    <img src={allSuperHeroes[7]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[7]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[7]['description']}</div>
                   </Slide>
                   <Slide index={8}>
                     <div className="carousel-hero-name">{allSuperHeroes[8]['name']}</div>
-                    <img src={allSuperHeroes[8]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[8]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[8]['description']}</div>
                   </Slide>
                   <Slide index={9}>
                     <div className="carousel-hero-name">{allSuperHeroes[9]['name']}</div>
-                    <img src={allSuperHeroes[9]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[9]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[9]['description']}</div>
                   </Slide>
                   <Slide index={10}>
                     <div className="carousel-hero-name">{allSuperHeroes[10]['name']}</div>
-                    <img src={allSuperHeroes[10]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[10]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[10]['description']}</div>
                   </Slide>
                   <Slide index={11}>
                     <div className="carousel-hero-name">{allSuperHeroes[11]['name']}</div>
-                    <img src={allSuperHeroes[11]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[11]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[11]['description']}</div>
                   </Slide>
                   <Slide index={12}>
                     <div className="carousel-hero-name">{allSuperHeroes[12]['name']}</div>
-                    <img src={allSuperHeroes[12]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[12]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[12]['description']}</div>
                   </Slide>
                   <Slide index={13}>
                     <div className="carousel-hero-name">{allSuperHeroes[13]['name']}</div>
-                    <img src={allSuperHeroes[13]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[13]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[13]['description']}</div>
                   </Slide>
                   <Slide index={14}>
                     <div className="carousel-hero-name">{allSuperHeroes[14]['name']}</div>
-                    <img src={allSuperHeroes[14]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[14]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[14]['description']}</div>
                   </Slide>
                   <Slide index={15}>
                     <div className="carousel-hero-name">{allSuperHeroes[15]['name']}</div>
-                    <img src={allSuperHeroes[15]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[15]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[15]['description']}</div>
                   </Slide>
                   <Slide index={16}>
                     <div className="carousel-hero-name">{allSuperHeroes[16]['name']}</div>
-                    <img src={allSuperHeroes[16]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[16]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[16]['description']}</div>
                   </Slide>
                   <Slide index={17}>
                     <div className="carousel-hero-name">{allSuperHeroes[17]['name']}</div>
-                    <img src={allSuperHeroes[17]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[17]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[17]['description']}</div>
                   </Slide>
                   <Slide index={18}>
                     <div className="carousel-hero-name">{allSuperHeroes[18]['name']}</div>
-                    <img src={allSuperHeroes[18]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[18]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[18]['description']}</div>
                   </Slide>
                   <Slide index={19}>
                     <div className="carousel-hero-name">{allSuperHeroes[19]['name']}</div>
-                    <img src={allSuperHeroes[19]['imageUrl']} className="carousel-hero-imageurl" />
+                    <img src={allSuperHeroes[19]['imageUrl']} className="carousel-hero-imageurl" alt="pic" />
                     <div className="carousel-hero-description">{allSuperHeroes[19]['description']}</div>
                   </Slide>
                 </Slider>
